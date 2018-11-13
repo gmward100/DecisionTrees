1327633fb2fdce08c1a2b31346e825b486c6e413
Geoff Ward <gward@arete.com>
2018-11-13 15:34:49 -0800

    Gini impurity version of random forest for one class is implemented but untested

--- RandomForest.py
+++ RandomForest.py
@@ -36,20 +36,86 @@ class RandomForest:
         self.oob_score = oob_score
         self.n_jobs = n_jobs
         self.random_state = random_state
-        self.split_feature_indicies = []
-        self.split_feature_values = []
+        self.tree_base_node = 0
         
-    def grow_tree(self,x,y,max_features):
-        print('growing')
+    class RFTreeNode:
+        def __init__(self):
+            self.split_feature_index = -1
+            self.split_feature_value = None
+            self.less_than_node = 0
+            self.greater_than_node = 0
+            self.prediction_value = None
+            self.depth = 0
+            
+        def grow_tree(self,x,y,max_features,criterion,min_samples_leaf,min_samples_split,max_depth,depth):
+            
+            self.depth = depth+1
+            if self.depth == max_depth:
+                self.prediction_value = np.mean(y)
+                return
+                
+            ySum = np.sum(y)
+            if ySum == 0 or ySum == y.shape[0]:
+                self.prediction_value = y[0]
+                return
+            
+            if x.shape[1] <= min_samples_split:
+                self.prediction_value = np.mean(y)
+                return
+            
+            feature_indicies = np.random.randint(0,x.shape[0],size=max_features)
+            if criterion == 'gini':
+                min_gini_index = -1
+                min_gini_argsort = np.zeros(x.shape[1])
+                min_gini_impurity = float(x.shape[1]+2)
+                count = np.arange(1.0,float(x.shape[1]+1))
+                for iFtr in feature_indicies:
+                    xArgSort=x[iFtr,:].argsort()
+                    xSorted = x[xArgSort]
+                    ySorted = y[xArgSort]
+                    pLeft = np.cumsum(ySorted)/count
+                    pRight = np.cumsum(ySorted[::-1]/count)[::-1]      
+                    giniImpurity = count*pLeft*(1.0-pLeft)+(float(xSorted.shape[1]+1)-count)*pRight*(1.0-pRight)
+                    indxArgMin = np.argmin(giniImpurity[min_samples_leaf-1:giniImpurity.shape[0]-min_samples_leaf-1])+min_samples_leaf-1
+                    if giniImpurity[indxArgMin] < min_gini_impurity:
+                        self.split_feature_value = xSorted[indxArgMin]
+                        self.split_feature_index = iFtr
+                        min_gini_impurity = giniImpurity[indxArgMin]
+                        min_gini_index = indxArgMin
+                        min_gini_argsort = xArgSort
+                x=x[:,min_gini_argsort]
+                y=y[min_gini_argsort]
+                self.less_than_node = self.RFTreeNode()
+                self.less_than_node.grow_tree(x[:,:min_gini_index+1],y[:min_gini_index+1],max_features,criterion,min_samples_leaf,min_samples_split,max_depth,self.depth)
+                self.greater_than_node = self.RFTreeNode()
+                self.less_than_node.grow_tree(x[:,min_gini_index+1:],y[min_gini_index+1:],max_features,criterion,min_samples_leaf,min_samples_split,max_depth,self.depth)     
+            else:
+                print('ENTROPY NOT IMPLEMENTED')
+            
+        def predict(self,x):
+            if self.prediction_value is not None:
+                return self.prediction_value
+            else:
+                if x[self.split_feature_index] <= self.split_feature_value:
+                    return self.less_than_node.predict(x)
+                else:
+                    return self.greater_than_node.predict(x)           
+                
         
     def fit(self,x,y):
         max_features = self.max_features
         if type(self.max_features) == str:
-            max_features = np.int32(np.ceil(np.sqrt(x.shape[1])))
+            max_features = np.int32(np.ceil(np.sqrt(x.shape[0])))
             
         np.random.seed(self.random_state)
 
         for iEstimator in range(self.n_estimators):
-            self.split_feature_indicies.append([])
-            self.split_feature_values.append([])
-            self.grow_tree(x,y,max_features)
+            self.tree_base_node = self.RFTreeNode()
+            self.tree_base_node.grow_tree(x,y,max_features,self.criterion,self.min_samples_leaf,self.min_samples_split,self.max_depth,0)
+            
+    def predict(self,x):
+        if len(x.shape) == 1:
+            return self.tree_base_node(x)
+        else:
+            #return np.apply_along_axis(self.tree_base_node.predict,axis=0,x)
+            return np.array([self.tree_base_node.predict(x[:,indx]) for indx in range(x.shape[1])])
